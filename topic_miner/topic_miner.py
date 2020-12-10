f4fbc2b8c984b872a124db945357e5504685f774
srasi1 <srasi1@gmail.com>
2020-12-09 22:34:23 -0500

    updates with extraction

--- topic_miner/topic_miner.py
+++ topic_miner/topic_miner.py
@@ -1,7 +1,10 @@
 import numpy as np
 import math
 import pandas as pd
-
+import nltk
+#from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
 
 class TelehealthMiner(object):
     def __init__(self, data_path):
@@ -10,6 +13,11 @@ class TelehealthMiner(object):
         """
         print('This is init function')
         self.data_path = data_path
+        self.pos_notes = []
+        self.no_pos_notes = []
+        self.vocabulary = []
+        self.number_of_pos_notes = 0
+        self.number_of_no_pos_notes = 0
 
         self.pos_en_path = data_path + 'positive_encounters.res-sample-50.res.csv'
         self.no_pos_en_path = data_path + 'no_positive_encounters.res-sample-50.res.csv'
@@ -19,6 +27,89 @@ class TelehealthMiner(object):
 
         self.pos_purpose_path = data_path + 'positive_purpose.txt'
         self.no_pos_purpose_path = data_path + 'no_positive_purpose.txt'
+
+    def clean_data(self, flag):
+        if flag == 0:
+            docs = self.pos_notes
+        else:
+            docs = self.no_pos_notes
+
+        clean_docs = []
+        nltk.download('wordnet')
+        stemmer = WordNetLemmatizer()
+        for dirty_doc in docs:
+            # remove phi redaction brackets
+            clean_doc = re.sub(r'\[\*\*.*?\*\*\]+', ' ', dirty_doc)
+
+            # remove special chars
+            clean_doc = re.sub(r'\W', ' ', clean_doc)
+
+            # remove all single chars
+            clean_doc = re.sub(r'\s+[a-zA-Z]\s+', ' ', clean_doc)
+
+            # remove single chars  from the start
+            clean_doc = re.sub(r'\^[a-zA-Z]\s+', ' ', clean_doc)
+
+            # replace multi space with single space
+            clean_doc = re.sub(r'\s+', ' ', clean_doc, flags=re.I)
+
+            # removing prefixed with 'b'
+            clean_doc = re.sub(r'^b\s+', '', clean_doc)
+
+            # conver to lowercase
+            clean_doc = clean_doc.lower()
+
+            # lemmatization
+            clean_doc = clean_doc.split()
+            clean_doc = [stemmer.lemmatize(word) for word in clean_doc]
+            clean_doc = ' '.join(clean_doc)
+
+            clean_docs.append(clean_doc)
+
+        return clean_docs
+
+    def clean_notes(self):
+        print("####*** Inside cleaning the notes ***###")
+        print(self.pos_notes[0])
+        self.pos_notes = self.clean_data(0)
+        self.no_pos_notes = self.clean_data(1)
+
+        print(self.pos_notes[0])
+
+    def corpus_builder(self, flag):
+        if flag == 0:
+            doc_path = self.pos_note_path
+            note = self.pos_notes
+            number = self.number_of_pos_notes
+        else:
+            doc_path = self.no_pos_note_path
+            note = self.no_pos_notes
+            number = self.number_of_no_pos_notes
+
+        fh = open(doc_path)
+        lines = fh.readlines()
+        for line in lines:
+            #print("line is: {}".format(line))
+            note.append(line.split())
+            number += 1
+        return note, number
+
+
+    def build_corpus(self):
+        """
+        Read document, fill in self.documents, a list of list of word
+        self.documents = [["the", "day", "is", "nice", "the", ...], [], []...]
+
+        Update self.number_of_documents
+        """
+        #self.pos_notes, self.number_of_pos_notes = self.corpus_builder(self.pos_note_path, self.pos_notes, self.number_of_pos_notes)
+        self.pos_notes, self.number_of_pos_notes = self.corpus_builder(0)
+        #self.no_pos_notes, self.number_of_no_pos_notes = self.corpus_builder(self.no_pos_note_path, self.no_pos_notes, self.number_of_no_pos_notes)
+        self.no_pos_notes, self.number_of_no_pos_notes = self.corpus_builder(1)
+
+    def build_vocabulary(self):
+        pos_vocabulary = []
+        no_pos_vocabulary = []
 
     def topic_miner(self):
         """
@@ -52,10 +143,12 @@ def main():
     This is the main function
     :return:
     """
-    data_path = '../data/'
+    data_path = '../patient_data/'
     # Write the main code here
     miner = TelehealthMiner(data_path)
     miner.extract_purpose_notes()
+    miner.build_corpus()
+    miner.clean_notes()
     miner.topic_miner()
 
 
