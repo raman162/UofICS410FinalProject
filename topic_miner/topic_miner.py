c4c29f5b2ac59be880c520abf349090664a84b45
srasi1 <srasi1@gmail.com>
2020-12-12 13:16:27 -0500

    corrected indentation and removed the commented out code

--- topic_miner/topic_miner.py
+++ topic_miner/topic_miner.py
@@ -4,14 +4,13 @@ import pandas as pd
 import collections
 import nltk
 import codecs
+import matplotlib
 from pylab import random
 from nltk.stem import WordNetLemmatizer
-#print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
-#from ..classifier import classifier as gm
-
 import re
 nltk.download('wordnet')
 nltk.download('stopwords')
+
 
 class TelehealthMiner(object):
     def __init__(self, input_arg_list):
@@ -42,6 +41,10 @@ class TelehealthMiner(object):
         self.topic_word_prob = None
 
     def clean_data(self):
+        """
+
+        :return:
+        """
         clean_docs = []
         stemmer = WordNetLemmatizer()
         for dirty_doc in self.notes_raw:
@@ -74,15 +77,13 @@ class TelehealthMiner(object):
 
             clean_docs.append(clean_doc)
         #print(clean_docs)
-
         return clean_docs
 
     def clean_notes(self):
         print("####*** Inside cleaning the notes ***###")
         self.clean_notes = self.clean_data()
         #print(self.clean_notes)
-
-       # print(self.pos_notes[0])
+        #print(self.pos_notes[0])
 
     def build_vocabulary (self):
         current_id = 0;
@@ -106,21 +107,20 @@ class TelehealthMiner(object):
         self.vocabulary_size = len(self.vocabulary)
         #print(self.word_frequencies)
         #print(self.vocabulary)
-        #print(self.vocabulary_size) 
-    
+        #print(self.vocabulary_size)
         
     def get_word_frequency(self):
-        #for i in range(0, len(self.vocabulary)): 
-         #   print('Word Frequency [{}]: {}'.format(self.vocabulary[i], self.vocabulary.count(self.vocabulary[i])))
-         word_frequencies_no_pos = collections.Counter(self.no_pos_vocabulary)
-         top_word_frequencies_no_pos = word_frequencies_no_pos.most_common(50)
-     #    print(top_word_frequencies_no_pos)
-         word_frequencies_pos = collections.Counter(self.pos_vocabulary)
-         top_word_frequencies_pos = word_frequencies_pos.most_common(50)
-       #  print(top_word_frequencies_pos)
-         word_frequencies = collections.Counter(self.vocabulary)
-         top_word_frequencies = word_frequencies.most_common()
-         print(top_word_frequencies)
+        # for i in range(0, len(self.vocabulary)):
+        # print('Word Frequency [{}]: {}'.format(self.vocabulary[i], self.vocabulary.count(self.vocabulary[i])))
+        word_frequencies_no_pos = collections.Counter(self.no_pos_vocabulary)
+        top_word_frequencies_no_pos = word_frequencies_no_pos.most_common(50)
+        # print(top_word_frequencies_no_pos)
+        word_frequencies_pos = collections.Counter(self.pos_vocabulary)
+        top_word_frequencies_pos = word_frequencies_pos.most_common(50)
+        # print(top_word_frequencies_pos)
+        word_frequencies = collections.Counter(self.vocabulary)
+        top_word_frequencies = word_frequencies.most_common()
+        print(top_word_frequencies)
         
         
     def topic_miner(self):
@@ -151,40 +151,27 @@ class TelehealthMiner(object):
             word = word.strip()
             self.stop_words_repo.append(word)
         stop_words_file.close()
-        return(self.stop_words_repo)
+        return self.stop_words_repo
 
     def build_term_doc_matrix(self):
-#        n = self.no_of_docs
-#        m = len(self.vocabulary)
-#        #self.pos_term_doc_matrix = np.zeros([n, m], dtype=np.int)
-#        #self.no_pos_term_doc_matrix = np.zeros([n, m], dtype=np.int)
-#        self.total_doc_matrix = np.zeros([n, m], dtype=np.int)
-#        for i in range(n):
-#            for j in range(m):
-#                #self.pos_term_doc_matrix[i][j] = self.pos_notes[i].count(self.vocabulary[j])
-#                #self.no_pos_term_doc_matrix[i][j] = self.no_pos_notes[i].count(self.vocabulary[j])
-#                self.total_doc_matrix[i][j] = self.total_notes[i].count(self.vocabulary[j])
-        
-         self.term_doc_matrix = np.zeros([self.no_of_docs, self.vocabulary_size], dtype=np.int8)
-         for word in self.vocabulary:
-             j = self.map_word_to_id[word]
-             for i in range(0, self.no_of_docs):
-                 if word in self.word_frequencies[i]:
-                     self.term_doc_matrix[i, j] = self.word_frequencies[i][word];  
-         #print(self.term_doc_matrix)
+        self.term_doc_matrix = np.zeros([self.no_of_docs, self.vocabulary_size], dtype=np.int8)
+        for word in self.vocabulary:
+            j = self.map_word_to_id[word]
+            for i in range(0, self.no_of_docs):
+                if word in self.word_frequencies[i]:
+                    self.term_doc_matrix[i, j] = self.word_frequencies[i][word];
+        # print(self.term_doc_matrix)
                 
     def initialize(self):
         self.topic_prob = np.zeros([self.no_of_docs, self.vocabulary_size, self.no_of_topics], dtype=np.float)
-        #self.document_topic_prob = normalize(np.random.random_sample((self.no_of_docs, self.no_of_topics)))
-        #self.topic_word_prob = normalize(np.random.random_sample((self.no_of_topics, self.vocabulary_size)))
         self.document_topic_prob = random((self.no_of_docs, self.no_of_topics))
-        #self.document_topic_prob = normalize(self.document_topic_prob)
+        # self.document_topic_prob = normalize(self.document_topic_prob)
         self.topic_word_prob = random((self.no_of_topics, self.vocabulary_size))
-        #self.topic_word_prob = normalize(self.topic_word_prob)
+        # self.topic_word_prob = normalize(self.topic_word_prob)
     
     def expectation_step(self):
         print("E step:")
-        #self.topic_prob = normalize(np.matmul(self.document_topic_prob, self.topic_word_prob))
+        # self.topic_prob = normalize(np.matmul(self.document_topic_prob, self.topic_word_prob))
         for d in range(self.no_of_docs):
             for z in range(self.no_of_topics):
                 for w in range(self.vocabulary_size):
@@ -195,11 +182,11 @@ class TelehealthMiner(object):
                 column_total = 0
                 for z in range(self.no_of_topics):
                     column_total += self.topic_prob[d][z][w]
-                    #print(column_total)
+                    # print(column_total)
                 for z in range(self.no_of_topics):
                     self.topic_prob[d][z][w] = (self.topic_prob[d][z][w] / column_total)
-       # print("topic_prob is: {}".format(self.topic_prob))
-        #print("shape of topic_prob is: {}".format(self.topic_prob.shape))
+        # print("topic_prob is: {}".format(self.topic_prob))
+        # print("shape of topic_prob is: {}".format(self.topic_prob.shape))
         
     def e_step(self):
         for i in range(self.no_of_docs):
@@ -215,25 +202,7 @@ class TelehealthMiner(object):
                     for k in range(0, self.no_of_topics):
                         self.topic_prob[i, j, k] /= denominator;
         
-        
     def m_step(self):
-#        print("M step:")
-#        print("building topic_word_prob matrix in M step ...")
-#        for w in range(self.vocabulary_size):
-#            for d in range(self.no_of_docs):
-#                for z in range(self.no_of_topics):
-#                    self.topic_word_prob[z][w] += self.total_doc_matrix[d][w] * self.topic_prob[d][z][w]
-#        self.topic_word_prob = normalize(self.topic_word_prob)
-#        #print("1st Matrix in M - topic_word_prob is built")
-#        #print("Topic_word_prob matrix is {}".format(self.topic_word_prob))
-#        for d in range(self.no_of_docs):
-#            for w in range(self.vocabulary_size):
-#                for z in range(self.no_of_topics):
-#                    self.document_topic_prob[d][z] += self.total_doc_matrix[d][w] * self.topic_prob[d][z][w]
-#        self.document_topic_prob = normalize(self.document_topic_prob)
-#        #print("2nd Matrix in M - document_topic_prob is built")
-#        #print("Document_topic_prob is {}".format(self.document_topic_prob))
-        
         for k in range(self.no_of_topics):
             denominator = 0
             for j in range(self.vocabulary_size):
@@ -250,37 +219,17 @@ class TelehealthMiner(object):
 
         for i in range(self.no_of_docs):
             for k in range(self.no_of_topics):
-               self.document_topic_prob[i, k] = 0
-               denominator = 0
-               for j in range(self.vocabulary_size):
-                   self.document_topic_prob[i, k] += self.term_doc_matrix[i, j] * self.topic_prob[i, j, k]
-                   denominator += self.term_doc_matrix[i, j];
-               if denominator == 0:
-                   self.document_topic_prob[i, k] = 1.0 / self.no_of_topics
-               else:
-                   self.document_topic_prob[i, k] /= denominator
-       # print("1st Matrix in M - topic_word_prob is built")
-       # print("Topic_word_prob matrix is {}".format(self.topic_word_prob))
-       # print("2nd Matrix in M - document_topic_prob is built")
-       # print("Document_topic_prob is {}".format(self.document_topic_prob))
-        
+                self.document_topic_prob[i, k] = 0
+                denominator = 0
+                for j in range(self.vocabulary_size):
+                    self.document_topic_prob[i, k] += self.term_doc_matrix[i, j] * self.topic_prob[i, j, k]
+                    denominator += self.term_doc_matrix[i, j];
+                if denominator == 0:
+                    self.document_topic_prob[i, k] = 1.0 / self.no_of_topics
+                else:
+                    self.document_topic_prob[i, k] /= denominator
+
     def calculate_likelihood(self):
-#        print("Calculating the Likelihood")
-#        likelihood_matrix = np.zeros([self.no_of_docs, self.vocabulary_size], dtype=np.float)
-#        x = np.matmul(self.document_topic_prob, self.topic_word_prob)
-#        doc_term_matrix = np.log(x)
-#        for d in range(self.no_of_docs):
-#            for w in range(self.vocabulary_size):
-#                likelihood_matrix[d][w] = self.total_doc_matrix[d][w] * doc_term_matrix[d][w]
-#        word_vocab = np.zeros([self.no_of_docs, 1], dtype=np.float)
-#        for d in range(self.no_of_docs):
-#            for w in range(self.vocabulary_size):
-#                word_vocab[d] += likelihood_matrix[d][w]
-#        likelihood = 0.0
-#        for d in range(self.no_of_docs):
-#            likelihood += word_vocab[d]
-#        print("likelihood is: {}".format(likelihood))
-#        return likelihood
         likelihood = 0
         for i in range(0, self.no_of_docs):
             for j in range(0, self.vocabulary_size):
@@ -295,7 +244,7 @@ class TelehealthMiner(object):
         print ("EM iteration begins...")
         current_likelihood = 1.0
         prev_likelihood = 1.0
-       # prev_likelihood = current_likelihood
+        # prev_likelihood = current_likelihood
         for iteration in range(self.no_of_iterations):
             print("Iteration #" + str(iteration + 1) + "...")
             self.e_step()
@@ -309,9 +258,7 @@ class TelehealthMiner(object):
                 break
             else:
                 prev_likelihood = current_likelihood
-               
-        
-        
+
     def normalize(self):
         for i in range(self.no_of_docs):
             normalization = sum(self.document_topic_prob[i, :])
@@ -322,7 +269,6 @@ class TelehealthMiner(object):
             normalization = sum(self.topic_word_prob[i, :])
             for j in range(self.vocabulary_size):
                 self.topic_word_prob[i, j] /= normalization;
-                
                 
     def print_files(self, topic_doc_fname, topic_word_fname, vocabulary_fname, topic_fname):
     # document-topic distribution
@@ -362,9 +308,6 @@ class TelehealthMiner(object):
             file.write(tmp + '\n')
         file.close()
 
-        
-
-        
 
 def main():
     """
@@ -373,9 +316,8 @@ def main():
     """
     file_path = '../patient_data/'
 
-    
     # Write the main code here
-    if(len(sys.argv) == 11):
+    if len(sys.argv) == 11:
         file_name = sys.argv[1]
         stop_words_file_name = sys.argv[2]
         no_of_topics = int(sys.argv[3])
@@ -411,7 +353,8 @@ def main():
     miner.normalize()
     miner.plsa()
     miner.print_files(topic_doc_distribution,topic_word_distribition,vocabulary_out,topics_out)
-   # miner.plsa(max_iteration,epsilon)
-    
+    # miner.plsa(max_iteration,epsilon)
+
+
 if __name__ == '__main__':
     main()
